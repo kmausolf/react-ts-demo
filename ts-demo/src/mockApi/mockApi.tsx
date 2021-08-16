c88000b8c43accfaac382c4065585825f943f276
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2021-08-16 13:59:24 -0700

    feat(mockApi): add signup function

--- ts-demo/src/mockApi/mockApi.tsx
+++ ts-demo/src/mockApi/mockApi.tsx
@@ -2,14 +2,36 @@ interface User {
   name: string;
 }
 
+interface AccountSignUpRequest {
+  username: string;
+  password: string;
+  favoriteFood?: string;
+}
+
+interface AccountDetails {
+  id: number;
+  username: string;
+  favoriteFood?: string;
+}
+
 const allUsers: User[] = [
   { name: "Luis" },
   { name: "Elias" },
   { name: "Connie" },
 ];
 
+const createAccount = async (
+  request: AccountSignUpRequest
+): Promise<AccountDetails> => {
+  return {
+      id: 1,
+      username: request.username,
+      favoriteFood: request?.favoriteFood 
+  };
+};
+
 const getAllUsers = async (): Promise<User[]> => {
   return allUsers;
 };
 
-export { getAllUsers };
+export { createAccount, getAllUsers };
