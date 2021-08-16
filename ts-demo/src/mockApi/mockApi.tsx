cb527ce68f134968e591483523b2c5bba2408ba4
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2021-08-16 14:20:21 -0700

    feat(mockApi): restructure interfaces

--- ts-demo/src/mockApi/mockApi.tsx
+++ ts-demo/src/mockApi/mockApi.tsx
@@ -1,36 +1,48 @@
-interface User {
-  name: string;
-}
-
 interface AccountSignUpRequest {
   username: string;
   password: string;
   favoriteFood?: string;
 }
 
-interface AccountDetails {
+interface IAccountDetails {
   id: number;
   username: string;
   favoriteFood?: string;
 }
 
-const allUsers: User[] = [
-  { name: "Luis" },
-  { name: "Elias" },
-  { name: "Connie" },
+class AccountDetails implements IAccountDetails {
+    // Default settings
+    id: number = 0;
+    username: string = '';
+    favoriteFood: string = '';
+
+    // Override the defaults if real data is present
+    constructor(configOverride?: Partial<IAccountDetails>) {
+        if(configOverride) {
+            Object.assign(this, configOverride);
+        }
+    }
+}
+
+const allUsers: IAccountDetails[] = [
+  new AccountDetails({ id: 1, username: "wholesome_keanu_chungus_100" }),
+  new AccountDetails({ id: 2, username: "king_yodaX419", favoriteFood: "pb&j" }),
+  new AccountDetails({ id: 3, username: "cheeto_dust-kun", favoriteFood: "cheetos" }),
 ];
 
+// Simulates successful account creation
+// Simply sends back what was sent with an ID added
 const createAccount = async (
   request: AccountSignUpRequest
 ): Promise<AccountDetails> => {
-  return {
-      id: 1,
-      username: request.username,
-      favoriteFood: request?.favoriteFood 
-  };
+    return new AccountDetails({
+        id: 4,
+        username: request.username,
+        favoriteFood: request?.favoriteFood
+    });
 };
 
-const getAllUsers = async (): Promise<User[]> => {
+const getAllUsers = async (): Promise<IAccountDetails[]> => {
   return allUsers;
 };
 
