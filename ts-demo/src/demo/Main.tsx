4de4816ae01ce610805343ad22e5d4f9ed8f78c3
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2021-08-17 14:39:39 -0700

    feat(app): add basic demo file

---
+++ ts-demo/src/demo/Main.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const PrimaryContainer = styled.div`
+    background: #d4b4d6;
+    border-radius: 25px;
+    display: flex;
+    flex-direction: column;
+    min-height: 40vh;
+    min-width: 60vw;
+    padding: 2vh 2vw 2vh 2vw;
+`;
+
+const InputContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1vh;
+    width: 20vw;
+`;
+
+const Input = styled.input`
+`;
+
+const Label = styled.label`
+`;
+
+const Button = styled.button``;
+
+const Main = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [favoriteFood, setFavoriteFood] = useState("");
+
+    const handleSignUp = (): void => {
+        //do the sign up
+    }
+
+    return(
+        <PrimaryContainer>
+            <InputContainer>
+                <Label>
+                    Username
+                </Label>
+                <Input type="text" onChange={(event) => setUsername(event.target.value)} value={username} />
+            </InputContainer>
+            <InputContainer>
+                <Label>
+                    Password
+                </Label>
+                <Input type="password" onChange={(event) => setPassword(event.target.value)} value={password} />
+            </InputContainer>
+            <InputContainer>
+                <Label>
+                    Favorite Food
+                </Label>
+                <Input type="text" onChange={(event) => setFavoriteFood(event.target.value)} value={favoriteFood} />
+            </InputContainer>
+            <InputContainer>
+                <Button onClick={handleSignUp} >Sign Up</Button>
+            </InputContainer>
+        </PrimaryContainer> 
+    );
+}
+
+export default Main;
