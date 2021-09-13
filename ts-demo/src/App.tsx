d20635ff0473e8ba86e7ab5264458b91b1a51e5f
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-09-13 15:01:43 -0700

    add persistence

--- ts-demo/src/App.tsx
+++ ts-demo/src/App.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import Demo from "./demo/Demo";
 import "./App.css";
 import { Provider } from "react-redux";
-import {store} from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 
 function App() {
   return (
     <Provider store={store}>
-      <Demo />
+      <PersistGate loading={null} persistor={persistor}>
+        <Demo />
+      </PersistGate>
+      
     </Provider>
   );
 }
