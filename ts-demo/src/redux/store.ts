7dc00e12d5ed8a389f62a507e188cd6ea0ed6098
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-09-13 14:43:23 -0700

    add persistence in store

--- ts-demo/src/redux/store.ts
+++ ts-demo/src/redux/store.ts
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import demoSlice from "../demo/demoSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedDemoSlice = persistReducer(persistConfig, demoSlice);
 
 export const store = configureStore({
   reducer: {
-    demo: demoSlice,
+    demo: persistedDemoSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
@@ -11,7 +21,10 @@ export const store = configureStore({
     }),
 });
 
+export let persistor = persistStore(store);
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
