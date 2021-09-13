ad5c342e9af950f222a959c9828c9d3264a50f47
Connie Lai <clai011@ucr.edu>
2021-09-13 11:18:39 -0700

    added middleware in configureStore

--- ts-demo/src/redux/store.js
+++ ts-demo/src/redux/store.js
@@ -1,8 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import demoSlice from "../demo/demoSlice";
+
 
 export default configureStore({
   reducer: {
     demo: demoSlice,
   },
+  middleware: (getDefaultMiddleware)=>{
+    getDefaultMiddleware({ 
+      // customize thunk
+      serializableCheck: false
+    }) 
+  }
 });
